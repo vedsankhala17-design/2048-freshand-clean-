2a4a6e22f5e564a165a046505adb5de778dcd07a
vedsankhala17-design <vedsankhala17@gmail.com>
2026-02-25 10:47:23 +0530

    Add console log to verify application.js initialization

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
-// Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
+  console.log("Application JS loaded");
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
