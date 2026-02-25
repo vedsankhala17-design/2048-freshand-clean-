f621c5d21daa0a11faecbd268c0e541b72e537e3
vedsankhala17-design <vedsankhala17@gmail.com>
2026-02-25 10:34:25 +0530

    Fix addRandomTile function to insert tiles into grid

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,9 +59,11 @@ GameManager.prototype.setup = function () {
 };
 
 // Set up the initial tiles to start the game with
-GameManager.prototype.addStartTiles = function () {
-  for (var i = 0; i < this.startTiles; i++) {
-    this.addRandomTile();
+GameManager.prototype.addRandomTile = function () {
+  if (this.grid.cellsAvailable()) {
+    var value = Math.random() < 0.9 ? 2 : 4;
+    var tile = new Tile(this.grid.randomAvailableCell(), value);
+    this.grid.insertTile(tile);
   }
 };
 
