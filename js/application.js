9ccf598faf47c43fb35a77591a6a084e44cc372c
vedsankhala17-design <vedsankhala17@gmail.com>
2026-02-25 10:50:54 +0530

    Test application.js loading

--- js/application.js
+++ js/application.js
@@ -1,3 +1,4 @@
+alert ("application.js loaded")
 window.requestAnimationFrame(function () {
   console.log("Application JS loaded");
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
